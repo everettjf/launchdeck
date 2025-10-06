fcd8fab064e8d1036add56e6cb76a7ceaa2351d9
everettjf <everettjf@live.com>
2025-10-05 22:21:07 -0700

    apple foundation models ready

--- test_fm.swift
+++
@@ -1,8 +0,0 @@
-import FoundationModels
-import Foundation
-
-@available(macOS 26.0, *)
-func testAPI() async throws {
-    // Try to discover what's in FoundationModels
-    let model = try await FoundationModels.Model()
-}
